69eab34954cb2fce7869e4fdb8e83a59f7aaf3cd
Visrut Lukhi <100529711+Visrut2703@users.noreply.github.com>
2024-12-04 15:06:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0',  // Make the server accessible externally
     port: 2345        // Specify the desired port (2345)
